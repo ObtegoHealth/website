fc655015a40fd0085fe1fbe78c6f4130581117fe
Josh Quintal <joshquin@gmail.com>
2025-12-24 23:48:57 -0500

    Remove static site, add language support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: "English",
+  "zh-Hans": "中文",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.signup": "Join the Waitlist",
+  },
+  "zh-hans": {
+    "nav.signup": "获取早期体验资格",
+  },
+} as const;
